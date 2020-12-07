3fc2ccc3a6fa48004e5dd0b626ea66f20c82866c
yikwing <yikwing14@126.com>
2020-12-07 10:38:00 +0800

    upgrade readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flutter_pigeon
+
+
+```shell script
+flutter pub run pigeon --input pigeon/pigeon_bean.dart --dart_out lib/pigeon.dart --java_out ../MyFlutterDemo/app/src/main/java/com/example/myflutterdemo/pigeon.java
+```
--- app/src/main/java/com/example/myflutterdemo/MainActivity.kt
+++ app/src/main/java/com/example/myflutterdemo/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.myflutterdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.myflutterdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), Pigeon.ApiToNative {
 
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
+        
         FlutterTools.preWarmFlutterEngine(this)
 
         Pigeon.ApiToNative.setup(
@@ -21,30 +25,26 @@ class MainActivity : AppCompatActivity(), Pigeon.ApiToNative {
         )
 
 
-        page_a.setOnClickListener {
+        binding.pageA.setOnClickListener {
             Log.d("this", "点击了")
 
             val data = Pigeon.SendMsg()
             data.msg = "原生给A传值"
 
             Pigeon.ApiToFlutter(FlutterTools.getsFlutterEngine().dartExecutor.binaryMessenger)
-                .toFlutterMsg(data) {
-
-                }
+                .toFlutterMsg(data) {}
 
             AndroidFlutterActivity.open(this, FlutterTools.ROUTE_PAGE_A)
         }
 
-        page_b.setOnClickListener {
+        binding.pageB.setOnClickListener {
             Log.d("this", "点击了")
 
             val data = Pigeon.SendMsg()
             data.msg = "原生给B传值"
 
             Pigeon.ApiToFlutter(FlutterTools.getsFlutterEngine().dartExecutor.binaryMessenger)
-                .toFlutterMsg(data) {
-
-                }
+                .toFlutterMsg(data) {}
 
             AndroidFlutterActivity.open(this, FlutterTools.ROUTE_PAGE_B)
         }
@@ -61,8 +61,8 @@ class MainActivity : AppCompatActivity(), Pigeon.ApiToNative {
     override fun toHostMsg(arg: Pigeon.CallBackMsg) {
 
         when (arg.page) {
-            "A" -> page_a.text = arg.callback
-            "B" -> page_b.text = arg.callback
+            "A" -> binding.pageA.text = arg.callback
+            "B" -> binding.pageB.text = arg.callback
             else -> throw Error("Not Find Router")
         }
 
